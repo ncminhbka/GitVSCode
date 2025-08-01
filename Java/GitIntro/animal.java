114bf9f9bf26ef3a9e42a2a8466e19a8be385288
Nguyen Cong Minh <ncminhbka@gmail.com>
2025-08-01 11:37:06 +0700

    hieu

--- Java/GitIntro/animal.java
+++ Java/GitIntro/animal.java
@@ -3,6 +3,10 @@ package Java.GitIntro;
 public class animal {
     public int age;
     public String name;
+    public animal() {
+        this.age = 0;
+        this.name = "Unnamed";
+    }
     public void makeSound() {
         System.out.println("Animal sound");
     }
