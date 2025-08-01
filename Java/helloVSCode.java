8e991164948a5a5056f5c7ccb634d0aa8badb00d
Nguyen Cong Minh <ncminhbka@gmail.com>
2025-08-01 11:02:21 +0700

    add helloVSCode

---
+++ Java/helloVSCode.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class helloVSCode {
+    public static void main(String[] args) {
+        System.out.println("Hello, VS Code!");
+        System.out.println("Welcome to Java programming.");
+    }
+}
